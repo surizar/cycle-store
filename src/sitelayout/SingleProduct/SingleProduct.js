1e30d32185f823dae7449cc950131691e6a979af
Jacob Solis <vjsu1@student.london.ac.uk>
2021-02-03 20:24:41 -0600

    Adding buy button

---
+++ src/sitelayout/SingleProduct/SingleProduct.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import classes from './SingleProduct.module.css';
+
+const SingleProduct = (props) => {
+
+    const [product, setProductState] = useState ({});
+    const productId = +props.match.params.productId;
+
+    const productList = [
+        {
+            id:0,
+            image:"https://m.media-amazon.com/images/I/61vTMLNYNEL._AC_UY218_.jpg",
+            headline:"Folding Bike",
+            description:"This is the folding bike",
+            price: "100",
+            size: "M",
+            color: "Red",
+            weight: "15kg",
+            brand: "Zizzo"
+        },
+        {
+            id:1,
+            image:"https://m.media-amazon.com/images/I/713-Rfn9leL._AC_UY218_.jpg",
+            headline:"Mountain Bike",
+            description:"This is the MTB",
+            price: "100",
+            size: "M",
+            color: "Red",
+            weight: "15kg",
+            brand: "Zizzo"
+        },
+        {
+            id:2,
+            image:"https://m.media-amazon.com/images/I/71E0T2-BoYL._AC_UY218_.jpg",
+            headline:"Super mountain bike",
+            description:"This is the super MTB",
+            price: "100",
+            size: "M",
+            color: "Red",
+            weight: "15kg",
+            brand: "Zizzo"
+        },
+        {
+            id:3,
+            image:"https://m.media-amazon.com/images/I/71JvhciY+gL._AC_UY218_.jpg",
+            headline:"Urban bike",
+            description:"This is the urban bike",
+            price: "100",
+            size: "M",
+            color: "Red",
+            weight: "15kg",
+            brand: "Zizzo"
+        },
+        {
+            id:4,
+            image:"https://m.media-amazon.com/images/I/71l1PZLD6yL._AC_UY218_.jpg",
+            headline:"Other bike",
+            description:"This is the other bike",
+            price: "100",
+            size: "M",
+            color: "Red",
+            weight: "15kg",
+            brand: "Zizzo"
+        },
+        {
+            id:5,
+            image:"https://m.media-amazon.com/images/I/81AnExLhyJL._AC_UY218_.jpg",
+            headline:"Special bike",
+            description:"This is the special bike",
+            price: "100",
+            size: "M",
+            color: "Red",
+            weight: "15kg",
+            brand: "Zizzo"
+        }        
+    ];
+
+    useEffect( () => {
+        const p  = (productList.filter( p=>p.id===productId))[0] || {};
+        setProductState({...p});
+    }, []);
+
+    let objectInfo = <div className={classes.productInfo}><h1>Product Not Found</h1></div>;
+
+    if ( Object.entries(product).length !== 0 ){
+        objectInfo = (
+            <div>
+                <h1>{product.headline}</h1>
+                <img src={product.image} />
+                <p>{product.description}</p>
+            </div>
+        )
+    }
+
+    return (
+        <>
+        <h1>Single Product</h1>
+        {objectInfo}
+        </>
+    )
+}
+
+export default SingleProduct;
